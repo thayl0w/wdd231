3ab29bb4f026e25fe6fb720c8ef3d83e2e798dfd
thayl0w <softmardevelopment@gmail.com>
2025-01-09 19:50:36 +0800

    marthayne

--- scripts/homepage.js
+++ scripts/homepage.js
@@ -16,11 +16,11 @@ menuToggle.addEventListener('click', () => {
 // Course Data and Filtering
 const courses = [
   { name: 'CSE 110', type: 'cse', completed: true },
-  { name: 'CSE 111', type: 'cse', completed: false },
+  { name: 'CSE 111', type: 'cse', completed: true },
   { name: 'WDD 130', type: 'wdd', completed: true },
   { name: 'CSE 210', type: 'wdd', completed: false },
   { name: 'WDD 231', type: 'wdd', completed: false },
-  { name: 'WDD 131', type: 'wdd', completed: false },
+  { name: 'WDD 131', type: 'wdd', completed: true },
 ];
 
 const courseCards = document.getElementById('course-cards');
